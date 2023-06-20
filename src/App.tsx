d00561939d266ea41c2994ce9725ea94bf78663f
Rintaro Fujita <rinpc356@gmail.com>
2023-06-21 01:40:15 +0900

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import QuizApp from "./QuizApp";
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <QuizApp />
+    </div>
+  );
+};
+
+export default App;
